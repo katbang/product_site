dcd810e1f2f41fe9a8f82bbfe4527fdfa5100e33
katbang <katr164m@stud.kea.dk>
2023-02-06 11:21:04 +0100

    product v1 done

---
+++ js/product.js
@@ -0,0 +1,15 @@
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".product h3").textContent =
+    product.productdisplayname;
+  document.querySelector(".brand").textContent = product.brandname;
+  document.querySelector(".color").textContent = product.basecolour;
+  document.querySelector(".price").textContent = `DKK ${product.price} ,-`;
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
